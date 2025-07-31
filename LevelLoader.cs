8ba2361b32abaa1be11a856a8e97ee7ca222415b
Drekj3 <dirtsock3@gmail.com>
2025-07-31 13:51:38 -0400

    Adds basic level changes

---
+++ LevelLoader.cs
@@ -0,0 +1,54 @@
+using Godot;
+using System;
+using System.Linq;
+
+public partial class LevelLoader : Node2D
+{
+    [Export] NodePath[] levelArrayNodePath;
+
+    Node2D[] levelArr;
+    Node2D activeLevel;
+    public override void _Ready()
+    {
+        levelArr = new Node2D[levelArrayNodePath.Length];
+        for (int i = 0; i < levelArr.Count(); i++)
+        {
+            levelArr[i] = GetNode<Node2D>(levelArrayNodePath[i]);
+            if (i != 0)
+            {
+                levelArr[i].ProcessMode = ProcessModeEnum.Disabled;
+                levelArr[i].Visible = false;
+            }
+        }
+        activeLevel = levelArr[0];
+        ReadyLevelExit(activeLevel);
+        
+    }
+
+    public override void _Process(double delta)
+    {
+
+    }
+
+    public void OnBodyEntered(Node2D body)
+    {
+        GD.Print("A");
+        GoToNextLevel(levelArr[1]);
+    }
+
+    public void GoToNextLevel(Node2D nextLevel)
+    {
+        activeLevel.ProcessMode = ProcessModeEnum.Disabled;
+        activeLevel.Visible = false;
+        activeLevel = nextLevel;
+        activeLevel.ProcessMode = ProcessModeEnum.Always;
+        activeLevel.Visible = true;
+        ReadyLevelExit(activeLevel);
+    }
+
+    public void ReadyLevelExit(Node2D level)
+    {
+        Area2D exit = level.GetNode<Area2D>("Exit");
+        exit.BodyEntered += OnBodyEntered;
+    }
+}
--- exit.cs
+++ exit.cs
@@ -3,15 +3,4 @@ using System;
 
 public partial class exit : Area2D
 {
-    public override void _Ready()
-    {
-        // var Collision2d = GetNode<CollisionShape2D>("CollisionShape");
-        // Collision2d.OnBodyEntered();
-        BodyEntered += OnBodyEntered;
-    }
-
-    private void OnBodyEntered(Node2D body)
-    {
-        GD.Print("Exit");
-    }
 }
